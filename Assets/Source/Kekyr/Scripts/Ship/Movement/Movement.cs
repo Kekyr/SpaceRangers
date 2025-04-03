0a902c0e36565a19cbc577fc669b5b2564a0d237
Mikhail Ostronosov <60397098+Kekyr@users.noreply.github.com>
2025-04-03 19:58:23 +0300

    Continued to make player movement

--- Assets/Source/Kekyr/Scripts/Ship/Movement/Movement.cs
+++ Assets/Source/Kekyr/Scripts/Ship/Movement/Movement.cs
@@ -4,21 +4,16 @@ using UnityEngine.InputSystem;
 
 namespace ShipBase
 {
-    [RequireComponent(typeof(PlayerInputRouter))]
-    [RequireComponent(typeof(Rigidbody2D))]
-    public class KeyboardMovement : MonoBehaviour
+    public class Movement : MonoBehaviour
     {
         private readonly string _movingAnimation = "IsMoving";
-        private readonly float _speed = 5f;
 
         [SerializeField] private Animator _engineAnimator;
-
+        
         private PlayerInputRouter _playerInputRouter;
         private Rigidbody2D _rigidbody;
         private ShipHealth _health;
-
-        private Vector3 _direction;
-
+        
         private bool _isMoving;
 
         private void Start()
@@ -31,52 +26,22 @@ namespace ShipBase
             _playerInputRouter = GetComponent<PlayerInputRouter>();
             _rigidbody = GetComponent<Rigidbody2D>();
             _health = GetComponent<ShipHealth>();
-
+            
             _health.Died += OnDead;
             _playerInputRouter.Move.performed += OnMovePerformed;
-            _playerInputRouter.Move.canceled += OnMoveCanceled;
         }
-
+        
         private void OnDisable()
         {
             _health.Died -= OnDead;
             _playerInputRouter.Move.performed -= OnMovePerformed;
-            _playerInputRouter.Move.canceled -= OnMoveCanceled;
         }
-
-        private void FixedUpdate()
-        {
-            if (_isMoving == true)
-            {
-                _rigidbody.velocity = _direction * _speed;
-            }
-        }
-
+        
         private void OnMovePerformed(InputAction.CallbackContext context)
         {
-            if (context.control.device is Touchscreen)
-            {
-                return;
-            }
 
-            Vector3 direction = context.ReadValue<Vector2>();
-            _direction = direction;
-            _isMoving = true;
-            _engineAnimator.SetBool(_movingAnimation, _isMoving);
         }
-
-        private void OnMoveCanceled(InputAction.CallbackContext context)
-        {
-            if (context.control.device is Touchscreen)
-            {
-                return;
-            }
-
-            _isMoving = false;
-            _engineAnimator.SetBool(_movingAnimation, _isMoving);
-            _rigidbody.velocity = Vector3.zero;
-        }
-
+        
         private void OnDead()
         {
             _engineAnimator.gameObject.SetActive(false);
