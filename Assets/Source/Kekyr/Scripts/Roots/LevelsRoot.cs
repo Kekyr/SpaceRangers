42683a4868d4dae00a5e7680f0608c06a71a2d11
Mikhail Ostronosov <60397098+Kekyr@users.noreply.github.com>
2025-05-21 14:18:53 +0300

    Polished planets prefabs

--- Assets/Source/Kekyr/Scripts/Roots/LevelsRoot.cs
+++ Assets/Source/Kekyr/Scripts/Roots/LevelsRoot.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
 
 public class LevelsRoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly float _initTime = 0.001f;
+    
+    [SerializeField] private RawImage _background;
+    [SerializeField] private ScreenAdjuster _screenAdjuster;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private PostProcessProfile _postProcessProfile;
+
+    private void Validate()
     {
-        
+        if (_background == null)
+        {
+            throw new ArgumentNullException(nameof(_background));
+        }
+
+        if (_screenAdjuster == null)
+        {
+            throw new ArgumentNullException(nameof(_screenAdjuster));
+        }
+
+        if (_canvas == null)
+        {
+            throw new ArgumentNullException(nameof(_canvas));
+        }
+
+        if (_camera == null)
+        {
+            throw new ArgumentNullException(nameof(_camera));
+        }
+
+        if (_postProcessProfile == null)
+        {
+            throw new ArgumentNullException(nameof(_postProcessProfile));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Validate();
+
+        _screenAdjuster.Init(_canvas, _camera, _background, _postProcessProfile);
+
+        StartCoroutine(Initialization());
+    }
+    
+    private IEnumerator Initialization()
+    {
+        yield return new WaitForSeconds(_initTime);
+        _screenAdjuster.ChangeBackground();
+        _screenAdjuster.ChangeEffect();
+        _screenAdjuster.enabled = true;
     }
 }
