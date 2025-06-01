4578ea86e9d14e50e6581a0ae11defcba507c563
Mikhail Ostronosov <60397098+Kekyr@users.noreply.github.com>
2025-06-01 16:59:43 +0300

    LeaderboardView is done

--- Assets/Source/Kekyr/Scripts/LeaderboardBase/Scripts/View/LeaderboardView.cs
+++ Assets/Source/Kekyr/Scripts/LeaderboardBase/Scripts/View/LeaderboardView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using YG;
 
 namespace LeaderboardBase
@@ -15,9 +16,12 @@ namespace LeaderboardBase
         [SerializeField] private LeaderboardElement _leaderboardElementPrefab;
         [SerializeField] private LeaderboardElement _playerLeaderboardElementPrefab;
 
+        [SerializeField] private Image _blackout;
+        [SerializeField] private Button _closeButton;
+
         private List<LeaderboardElement> _spawnedElements = new();
 
-        private void OnEnable()
+        private void Start()
         {
             if (_container == null)
             {
@@ -33,6 +37,23 @@ namespace LeaderboardBase
             {
                 throw new ArgumentNullException(nameof(_leaderboardElementPrefab));
             }
+
+            if (_blackout == null)
+            {
+                throw new ArgumentNullException(nameof(_blackout));
+            }
+
+            if (_closeButton == null)
+            {
+                throw new ArgumentNullException(nameof(_closeButton));
+            }
+            
+            _closeButton.onClick.AddListener(Close);
+        }
+
+        private void OnDestroy()
+        {
+            _closeButton.onClick.RemoveListener(Close);
         }
 
         public void ConstructLeaderboard(List<LeaderboardPlayer> leaderboardPlayers)
@@ -80,6 +101,12 @@ namespace LeaderboardBase
             }
 
             _spawnedElements = new List<LeaderboardElement>();
+        }
+
+        private void Close()
+        {
+            _blackout.gameObject.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
