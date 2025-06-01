eac6891f3c8f27857dc88041deb660693f9bc17d
Mikhail Ostronosov <60397098+Kekyr@users.noreply.github.com>
2025-06-01 14:28:47 +0300

    Created leaderboard view

--- Assets/Source/Kekyr/Scripts/UI/Buttons/LeaderboardButton.cs
+++ Assets/Source/Kekyr/Scripts/UI/Buttons/LeaderboardButton.cs
@@ -1,27 +1,51 @@
 using System;
 using LeaderboardBase;
 using UnityEngine;
+using UnityEngine.UI;
 using YG;
 
 namespace UI
 {
-    public class LeaderboardButton
+    public class LeaderboardButton : MonoBehaviour
     {
         [SerializeField] private Leaderboard _leaderboard;
         [SerializeField] private AuthorizationPopup _authorizationPopup;
+        [SerializeField] private Image _blackout;
 
-        private void OnEnable()
+        private Button _button;
+
+        private void Start()
         {
             if (_leaderboard == null)
             {
                 throw new ArgumentNullException(nameof(_leaderboard));
             }
+
+            if (_authorizationPopup == null)
+            {
+                throw new ArgumentNullException(nameof(_authorizationPopup));
+            }
+
+            if (_blackout == null)
+            {
+                throw new ArgumentNullException(nameof(_blackout));
+            }
+
+            _button = GetComponent<Button>();
+            
+            _button.onClick.AddListener(OnClick);
         }
 
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OnClick);
+        }
+        
         private void OnClick()
         {
             if (YandexGame.auth == false)
             {
+                _blackout.gameObject.SetActive(true);
                 _authorizationPopup.gameObject.SetActive(true);
                 return;
             }
--- Assets/Source/Kekyr/Scripts/UI/Popup/AuthorizationPopup.cs
+++ Assets/Source/Kekyr/Scripts/UI/Popup/AuthorizationPopup.cs
@@ -9,6 +9,8 @@ namespace UI
     public class AuthorizationPopup : MonoBehaviour
     {
         [SerializeField] private Button _signInButton;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private Image _blackout;
 
         private Leaderboard _leaderboard;
 
@@ -19,17 +21,30 @@ namespace UI
                 throw new ArgumentNullException(nameof(_signInButton));
             }
 
+            if (_closeButton == null)
+            {
+                throw new ArgumentNullException(nameof(_closeButton));
+            }
+
+            if (_blackout == null)
+            {
+                throw new ArgumentNullException(nameof(_blackout));
+            }
+
             _signInButton.onClick.AddListener(SignIn);
+            _closeButton.onClick.AddListener(Close);
         }
 
         private void OnDisable()
         {
             _signInButton.onClick.RemoveListener(SignIn);
+            _closeButton.onClick.RemoveListener(Close);
         }
 
         public void Init(Leaderboard leaderboard)
         {
             _leaderboard = leaderboard;
+            enabled = true;
         }
 
         private void SignIn()
@@ -44,6 +59,12 @@ namespace UI
                 gameObject.SetActive(false);
                 _leaderboard.Fill();
             }
+        }
+
+        private void Close()
+        {
+            _blackout.gameObject.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
